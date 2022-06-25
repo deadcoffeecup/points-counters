d310c38991fc608d0e2eac4f501e815ae2dd4838
deadcoffeecup <tsikora555@gmail.com>
2022-06-25 16:13:50 +0200

    delete negative points, added sum func

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import Counter from './Counter';
 import './App.css';
 
 function App() {
+  const [sum, setSum] = useState([0, 0, 0, 0]);
+  function handleSum(id, value) {
+    let newArr = [...sum];
+    newArr[id] = value;
+    setSum(newArr);
+    console.log(id, value);
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <Counter type={0.5} desc={'Cena 99-119'} />
-        <Counter type={1} desc={'Cena 120-149'} />
-        <Counter type={1.5} desc={'Cena > 150'} />
+        <Counter id={0} handleSum={handleSum} type={0.5} desc={'Cena 99-120'} />
+        <Counter id={1} handleSum={handleSum} type={1} desc={'Cena 121-150'} />
+        <Counter id={2} handleSum={handleSum} type={1.5} desc={'Cena > 151'} />
+        <Counter id={3} handleSum={handleSum} type={2} desc={'Cena > 200'} />
+        Suma:{' '}
+        {sum.reduce((current, next) => {
+          return current + next;
+        })}
       </header>
     </div>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const Counter = ({ type, desc }) => {
+const Counter = ({ id, type, desc, handleSum }) => {
   const [counter, setCounter] = React.useState(0);
+  const [value, setValue] = React.useState(0);
+
   const handleClick = (mode) => {
     mode ? setCounter((prev) => prev + 1) : setCounter((prev) => prev - 1);
   };
+  React.useEffect(() => {
+    counter < 0 && setCounter(0);
+    setValue(counter * type);
+  }, [counter, type]);
+  React.useEffect(() => {
+    handleSum(id, value);
+  }, [value]);
   return (
     <>
       <div>{desc}</div>
-      <div> ilość pozycji:{counter}</div>
+      <div> ilość punktów: {type * counter}</div>
       <div className='counter'>
         <button onClick={() => handleClick(false)}>-</button>
-        <div>{type * counter}</div>
+        <div>{counter}</div>
         <button onClick={() => handleClick(true)}>+</button>
       </div>
     </>
